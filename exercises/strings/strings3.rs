4ae32b5d8de1f5899776414591a8d32b7eb3a6f5
zwy2333 <108123374+zwy2333@users.noreply.github.com>
2023-10-12 09:38:23 +0000

    strings completed

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,47 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let start=get_start(input);
+    let end  =get_end(input)+1;
+    input[start..end].to_string()
+    //input.to_string().trim().to_string()
+}
+fn get_start(input: &str)->usize{
+    let bytes = input.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item != b' ' {
+            return i;
+        }
+    }
+    input.len()
+}
+fn get_end(input: &str)->usize{
+    let bytes = input.as_bytes();
+    for (i, &item) in bytes.iter().enumerate().rev() {
+            if item != b' ' {
+                return i;
+            }
+    }
+    input.len()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let app=String::from(" world!");
+    input.to_owned()+&app
+    
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]
